2f148450d8aaeb6d9374e0dc277f247ea3ec2842
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-08-02 21:27:00 +1000

    Add: Step 2

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+lint:
+	./vendor/bin/phpcs --standard=PSR12 src
+
+lint-fix:
+	./vendor/bin/phpcbf --standard=PSR12 src
+
 PORT ?= 8000
 start:
 	PHP_CLI_SERVER_WORKERS=5 php -S 0.0.0.0:$(PORT) -t public
--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+use DI\Container;
 
 $autoloadPath1 = __DIR__ . '/../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
@@ -15,18 +17,23 @@ if (file_exists($autoloadPath1)) {
 
 $app = AppFactory::create();
 
-// Добавление базового маршрута
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello, World!");
-    return $response;
+// Создание контейнера
+$container = new Container();
+AppFactory::setContainer($container);
+
+// Настройка PHP-View
+$container->set('renderer', function () {
+    return new PhpRenderer('../templates');
 });
 
-// Пример маршрута с параметром
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name!");
-    return $response;
+$app = AppFactory::create();
+
+// Маршрут для главной страницы
+$app->get('/', function (Request $request, Response $response, $args) {
+    $renderer = $this->get('renderer');
+    return $renderer->render($response, 'index.phtml', $args);
 });
 
 // Запуск приложения
 $app->run();
+
